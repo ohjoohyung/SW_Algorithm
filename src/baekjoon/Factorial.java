887e239a0ae6d67076036d61e0f76e105ab0e138
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-09-16 00:08:06 +0900

    백준 팩토리얼

---
+++ src/baekjoon/Factorial.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    //재귀함수 문제
+    //0! == 1
+    public static int factorial(int number) {
+        if(number == 1 || number == 0) {
+            return 1;
+        }
+       return number * factorial(number-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(factorial(number));
+    }
+}

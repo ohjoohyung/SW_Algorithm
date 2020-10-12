1b8825ef11b76f5faee9c2d95e0ea6f63bf65ba0
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-10-12 19:01:37 +0900

    백준 2941, 1316

--- src/baekjoon/String_Test.java
+++ src/baekjoon/String_Test.java
@@ -5,7 +5,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class String_Test {
     public static void main(String[] args) throws IOException {
@@ -104,23 +106,89 @@ public class String_Test {
 
         //백준 5622번
         //밑에 입력받아서 답 구하는거에서 이중for문을 썼었는데 그렇게 할 필요없이 한번에 해결할 수 있도록 바꾸었다
-        int[] arr = new int[26];
-        int val = 2;
-        for(int i = 0; i < 26; i++) {
-            arr[i] = val;
-            if((i+1) % 3 == 0 && i+1 <= 15) {
-                val++;
-            }else if(i+1 == 19 || i+1 == 22) {
-                val++;
+//        int[] arr = new int[26];
+//        int val = 2;
+//        for(int i = 0; i < 26; i++) {
+//            arr[i] = val;
+//            if((i+1) % 3 == 0 && i+1 <= 15) {
+//                val++;
+//            }else if(i+1 == 19 || i+1 == 22) {
+//                val++;
+//            }
+//        }
+//        String s = br.readLine();
+//        int answer = 0;
+//        for(int i = 0; i < s.length(); i++) {
+//            answer += arr[s.charAt(i)-'A']+1;
+//        }
+//
+//        System.out.println(answer);
+
+        //백준 2941번
+//        String s = br.readLine();
+//        String[] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+//
+//        int index = 0;
+//        int count = 0;
+//        while (index < arr.length) {
+//            if(s.contains(arr[index])) {
+//                s = s.replaceFirst(arr[index], " ");
+//                count++;
+//            }else {
+//                index++;
+//            }
+//            System.out.println(index + "/" + s);
+//        }
+//        for(int i = 0; i < s.length(); i++) {
+//            if(s.charAt(i) != ' ') {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        //백준 1316번
+        //이중for문으로 풀었는데 다른 사람 풀이를 보니 크기가 26인 boolean 배열을 만들어서 중복값을 체크하더라
+        //그럼 for문을 하나만 써도 됨
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+//        boolean check = true;
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+//            char[] cArr = s.toCharArray();
+            boolean answer = true;
+            boolean[] checkArr = new boolean[26];
+            char prev = ' ';
+            for(int j = 0; j < s.length(); j++) {
+                boolean check = checkArr[s.charAt(j)-'a'];
+                if(!check || prev == s.charAt(j)) {
+                    checkArr[s.charAt(j)-'a'] = true;
+                }else {
+                    answer = false;
+                    break;
+                }
+                prev = s.charAt(j);
+
             }
+            if(answer) count++;
+//            if(cArr.length != 1) {
+//                for(int j = 0; j < cArr.length; j++) {
+//                    for(int l = j + 1; l < cArr.length; l++) {
+//                        if(cArr[j] == cArr[l] && l-j == 1) {
+//                            break;
+//                        }else if(cArr[j] == cArr[l] && l-j > 1) {
+//                            check = false;
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//            if(check) count++;
+//            check = true;
         }
-        String s = br.readLine();
-        int answer = 0;
-        for(int i = 0; i < s.length(); i++) {
-            answer += arr[s.charAt(i)-'A']+1;
-        }
 
-        System.out.println(answer);
+        System.out.println(count);
 
 
-    }}
+
+    }
+}

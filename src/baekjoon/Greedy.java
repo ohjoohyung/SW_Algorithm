2c22e8c7f42686ad6d8a005960ed298a54df8c69
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-10-19 21:57:20 +0900

    백준 11399

--- src/baekjoon/Greedy.java
+++ src/baekjoon/Greedy.java
@@ -45,34 +45,34 @@ public class Greedy {
         //핵심은 endTime이 낮은 순서로 정렬하는 것
         //내 생각이지만 아마 String 배열로 만들었다가 메모리 초과가 뜬거같다
         //그래서 검색 후 이차원배열로 만듬
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][2];
-        for(int i = 0; i < n; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
-        }
-
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] first, int[] next) {
-                if(first[1] == next[1]) {
-                    return Integer.compare(first[0], next[0]);
-                }
-                return Integer.compare(first[1], next[1]);
-            }
-        });
-
-        int max = 1;
-        int endTime = arr[0][1];
-        for(int i = 1; i < n; i++) {
-            int startTime = arr[i][0];
-            if(startTime >= endTime) {
-                endTime = arr[i][1];
-                max++;
-            }
-        }
-        System.out.println(max);
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[][] arr = new int[n][2];
+//        for(int i = 0; i < n; i++) {
+//            arr[i][0] = sc.nextInt();
+//            arr[i][1] = sc.nextInt();
+//        }
+//
+//        Arrays.sort(arr, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] first, int[] next) {
+//                if(first[1] == next[1]) {
+//                    return Integer.compare(first[0], next[0]);
+//                }
+//                return Integer.compare(first[1], next[1]);
+//            }
+//        });
+//
+//        int max = 1;
+//        int endTime = arr[0][1];
+//        for(int i = 1; i < n; i++) {
+//            int startTime = arr[i][0];
+//            if(startTime >= endTime) {
+//                endTime = arr[i][1];
+//                max++;
+//            }
+//        }
+//        System.out.println(max);
 
 
         //for(int i = 0; i < n; i++) {
@@ -96,6 +96,22 @@ public class Greedy {
 //                }
 //            }
         //}
+
+        //11399번
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int min = 0;
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum += arr[i];
+            min += sum;
+        }
+        System.out.println(min);
 
 
     }

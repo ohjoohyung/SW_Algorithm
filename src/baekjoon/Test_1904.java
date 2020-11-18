b40e1eeb6c333bf8deee49cc4dea3786ae5c0797
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-18 21:53:29 +0900

    백준 1904

---
+++ src/baekjoon/Test_1904.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test_1904 {
+    //DP문제 메모리제이션 활용
+    //N>=2일때만 arr[2]=2 가 되도록 해줌으로써 런타임에러 해결
+    static final int NUM = 15746;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N+1];
+        arr[1] = 1;
+        if(N>=2) {
+            arr[2] = 2;
+        }
+        for(int i=3; i<=N; i++) {
+            arr[i] = (arr[i-1] + arr[i-2])%NUM;
+        }
+
+        System.out.println(arr[N]);
+
+
+    }
+
+
+}

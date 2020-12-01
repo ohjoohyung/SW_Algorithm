1ea8f591cc47d105e3092f390734e5b7bbb0d25d
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-01 21:26:24 +0900

    백준 5086

---
+++ src/baekjoon/Mathematics3.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Mathematics3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0) break;
+            String answer = "neither";
+            if(b % a == 0) answer = "factor";
+            if(a % b == 0) answer = "multiple";
+            System.out.println(answer);
+        }
+    }
+}

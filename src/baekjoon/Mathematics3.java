81781cf9039872323cab8dffa9f44b032831230b
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-11 19:48:45 +0900

    add: 백준 11653

--- src/baekjoon/Mathematics3.java
+++ src/baekjoon/Mathematics3.java
@@ -22,13 +22,30 @@ public class Mathematics3 {
 //        }
 
         //1037번
+//        int N = Integer.parseInt(br.readLine());
+//        int[] arr = new int[N];
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        for(int i=0; i<N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//        Arrays.sort(arr);
+//        System.out.println(arr[0]*arr[N-1]);
+
+        //11653번
+        //소인수분해 문제
+        //그냥 2부터 시작해서 나누어 떨어지는 수만 출력하면 된다.
+        //난 소수를 구하고 그걸 이용해 인수를 판별해 소인수를 구해야한다고 생각해 복잡하게 풀었는데
+        //그럴 필요없이 2부터 시작해서 나누기만 하면된다.
+        //단, 1일때는 출력을 해줄 필요조차 없다
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int index = 2;
+        while (N >= index) {
+            if (N % index == 0) {
+                System.out.println(index);
+                N /= index;
+            }else {
+                index++;
+            }
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0]*arr[N-1]);
     }
 }

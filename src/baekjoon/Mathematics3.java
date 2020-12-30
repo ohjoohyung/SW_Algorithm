cd72eb10be19d75da0acef0da59dac6a39e5985a
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-12-30 22:38:09 +0900

    add: 백준 2004

--- src/baekjoon/Mathematics3.java
+++ src/baekjoon/Mathematics3.java
@@ -145,28 +145,65 @@ public class Mathematics3 {
         //1676번
         //이렇게 풀었는데 굳이 10을 따지지 않고 2와 5만 따져도 똑같은 답이 나온다.
         //그래서 10을 뺀 걸로 수정
-        int n = Integer.parseInt(br.readLine());
-        int answer = 0;
-        int[] count = new int[2];
-        for (int i = n; i >= 1; i--) {
-            int tmpNumber = i;
-            while (true) {
-                if (tmpNumber % 2 == 0) {
-                    count[0]++;
-                    tmpNumber /= 2;
-                }else if (tmpNumber % 5 == 0) {
-                    count[1]++;
-                    tmpNumber /= 5;
-                }
+//        int n = Integer.parseInt(br.readLine());
+//        int answer = 0;
+//        int[] count = new int[2];
+//        for (int i = n; i >= 1; i--) {
+//            int tmpNumber = i;
+//            while (true) {
+//                if (tmpNumber % 2 == 0) {
+//                    count[0]++;
+//                    tmpNumber /= 2;
+//                }else if (tmpNumber % 5 == 0) {
+//                    count[1]++;
+//                    tmpNumber /= 5;
+//                }
+//
+//                if (tmpNumber % 2 != 0 && tmpNumber % 5 != 0) {
+//                    break;
+//                }
+//            }
+//        }
+//        answer += Math.min(count[0], count[1]);
+//        System.out.println(answer);
 
-                if (tmpNumber % 2 != 0 && tmpNumber % 5 != 0) {
-                    break;
-                }
-            }
-        }
-        answer += Math.min(count[0], count[1]);
+
+        //2004번
+        //2와 5 개수를 카운트 해주기 위해서 그냥 나눠서 나오는 몫만 더하면 됨
+        //int로 했다가 범위를 초과해서 전부 long으로 바꿔 해결
+        //처음 했던건 시간초과가 떠서 검색 후 해결
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        long twoCount = 0;
+        long fiveCount = 0;
+        twoCount += countTwo(n);
+        fiveCount += countFive(n);
+
+        twoCount -= countTwo(m);
+        fiveCount -= countFive(m);
+
+        twoCount -= countTwo(n-m);
+        fiveCount -= countFive(n-m);
+
+        long answer = Math.min(twoCount, fiveCount);
         System.out.println(answer);
+    }
 
+    static long countTwo(long number) {
+        long count = 0;
+        for (long i = 2; i <= number; i *= 2) {
+            count += number / i;
+        }
+        return count;
+    }
+
+    static long countFive(long number) {
+        long count = 0;
+        for (long i = 5; i <= number; i *= 5) {
+            count += number / i;
+        }
+        return count;
     }
 
 
